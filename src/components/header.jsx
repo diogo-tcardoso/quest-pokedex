cc60052634bc3cfa86390bd970fcb5298a87240a
diogo.tcardoso <diocardoso@hotmail.com>
2025-02-17 20:15:33 -0300

    Page name aand logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
 import ThemeSwitch from './theme-switch/theme-switch'
 import '../assets/styles/css/header.css'
+import logo from '../assets/img/pokedex-logo.png'
 export const Header = () => {
 
     return (
         <>
             <header>
-                <img src="../src/assets/img/pokedex-logo.png" alt="pokedex-logo"></img>
+                <img src= { logo } alt="pokedex-logo"></img>
                 <ThemeSwitch />
             </header>
         </>
